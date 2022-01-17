e1e2138500e8780fbb71db9d8b52a307f597b407
Velthas <damianobattaglia97@gmail.com>
2022-01-17 13:10:05 +0100

    Game can now be played and restarted

--- script.js
+++ script.js
@@ -4,30 +4,32 @@ function playerFactory (name, sign, isTurn) {
 
 const gameBoard = (function () {
 
-    const boardContent = ["","","","","","","","",""]
+    let boardContent = ["","","","","","","","",""]
 
-    function plugIntoBoard (index, playerSign) {
+    function plugIntoBoard(index, playerSign) {
         boardContent[index] = playerSign;
     }
 
-    function undoEventListeners () {
-        return;
-    }
-
-    function getBoardContent (index) {
+    function getBoardContent(index) {
         return boardContent[index];
-
     }
 
-    return {plugIntoBoard, getBoardContent}
+    function resetBoard() {
+        boardContent = boardContent.map(value => value = "");
+    }
+
+    return {plugIntoBoard, getBoardContent, resetBoard}
 
 })();
 
 const gameFlow = (function () {
 
+    //Variable will be used to determine if game has been won
+    let gameIsOver = false;
+
     //Get the two players
-    player1 = playerFactory('Player 1', "X", true);
-    player2 = playerFactory('Player 2', "O", false);
+    let player1 = playerFactory('Player 1', "X", true);
+    let player2 = playerFactory('Player 2', "O", false);
 
     //Winning combinations
     winConditions = [
@@ -41,18 +43,27 @@ const gameFlow = (function () {
         [2, 4, 6],
     ]
 
+    //Game can't be won before 5 turns.
+    //Run checks after that to improve performance.
     let turnCounter = 0;
-
 
     //Allow users to play their turn
     function playTurn(index, node) {
-        if(node.textContent !== '') return;
-        player1.isTurn === true ? node.textContent = player1.sign : node.textContent = player2.sign;
-        gameBoard.plugIntoBoard(index, node.textContent);
-        turnOver();
 
+        if(node.textContent !== '' || gameIsOver === true) return;
+
+        player1.isTurn === true ? node.textContent = player1.sign : node.textContent = player2.sign;
+        activePlayerSign = player1.isTurn === true ? player2.sign : player1.sign;
+
+        displayController.commentGame(activePlayerSign);
+        gameBoard.plugIntoBoard(index, node.textContent);
+
+        turnOver();
         turnCounter += 1
+        
         if (turnCounter > 4) checkWinConditions();
+        if (turnCounter === 9 && gameIsOver === false) displayController.commentGame('Tie');
+
     }
 
 
@@ -75,55 +86,94 @@ const gameFlow = (function () {
     function checkWinConditions() {
 
         let counter = 0;
-        let sign = "test";
+        let sign = "";
         
         //Iterate over all the win combinations
         for(let i = 0; i < winConditions.length; i++) {
 
+            //Always reset on each run
             counter = 0;
-            //In case of winning match all symbols will be the same, so just extract the first one to run checks.
+
+            //Sign on the first position will be repeated three times for a win
             let firstSignPosition = winConditions[i][0];
-            //If there is nothing on the first position, the win condition is unmatchable, so go to the next one.
+
+            //If there is nothing, win condition is impossible
             if (gameBoard.getBoardContent(firstSignPosition) !== "") {
                 sign = gameBoard.getBoardContent(firstSignPosition)
             }
             else continue;
 
-            //The same sign has to be on all three positions to get a win, so use a counter to verify it is the case
+            //Check if same repeats three times
             for(let j = 0; j < 3; j++) {
 
                 let position = winConditions[i][j];
+
                 if(gameBoard.getBoardContent(position) === sign) {
-                    counter += 1
-                }
+                    counter += 1;
+                } 
+ 
             }
 
+            //End the round
             if (counter === 3) {
-                console.log('We have a winner')
+                displayController.showWinner(sign);
+                gameIsOver = true;
             }
         }
     }
 
-    return {playTurn}
+    //Cleans up the board after a round is played
+    function startNewRound() {
+        gameBoard.resetBoard();
+        displayController.cleanBoard();
+
+        turnCounter = 0;
+        gameIsOver = false;
+
+        if (player1.isTurn === false) turnOver();
+        displayController.commentGame("X");
+    }
+
+    return {playTurn, startNewRound}
 })();
 
 const displayController = (function () {
 
     //Cache DOM
-    const board = document.querySelector('#gameboard');
+    const main = document.querySelector('#main');
+    const reset = main.querySelector('#reset');
+    const board = main.querySelector('#gameboard');
+    const gameFlowNarrator = main.querySelector('#gamestatus');
     const allTiles = Array.from(board.querySelectorAll('.socket'));
 
     function bindEventListeners() {
 
         for(let i = 0; i < allTiles.length; i++) {
-            allTiles[i].addEventListener('click', () => (function (node, tile) {return gameFlow.playTurn(node, tile)})(i, allTiles[i]));
+            allTiles[i].addEventListener('click', testHolder = () => (function (node, tile) {return gameFlow.playTurn(node, tile)})(i, allTiles[i]));
         }
+
+        reset.addEventListener('click', gameFlow.startNewRound);
+
     }
 
-    //TODO: Add function to show who has won
+    function showWinner(sign) {
+        gameFlowNarrator.textContent = `Player ${sign} won the game!`;
+    }
 
-    //TODO: Possibly add reset button
+    function commentGame(sign) {
+        if (sign === "Tie") {
+            gameFlowNarrator.textContent = "It's a draw!";
+            return;
+        }
+        gameFlowNarrator.textContent = `Player ${sign}'s turn!`;
+    }
 
+    function cleanBoard() {
+        allTiles.forEach(tile => tile.textContent = "");
+    }
+    
     bindEventListeners();
+
+    return {showWinner, cleanBoard, commentGame};
 
 })();
